8e3959d17973ac3c78f0c8945e55c438ccccc116
André Felipe Fuck <66512647+andreFelipeHFuck@users.noreply.github.com>
2024-02-22 15:33:04 -0300

    FAQs challange

---
+++ script.js
@@ -0,0 +1,29 @@
+const faqBt = document.querySelectorAll('.faq__button');
+const faqIcon = document.querySelectorAll('.faq__icon');
+const faqAnswer = document.querySelectorAll('.faq__answer');
+
+for(let i=0; i<faqBt.length; i++){
+    mostraOuDesmostraResposta(i)
+}
+
+function mostraOuDesmostraResposta(item){
+    faqBt[item].addEventListener('click', () => {
+        const valorSrc = faqIcon[item].getAttribute('src').split('/')[3];
+        if(valorSrc === "icon-plus.svg"){
+            showAnswer(item);
+        }else{
+            removeAnswer(item);
+        }
+    });    
+}
+
+function removeAnswer(item) {
+    faqIcon[item].setAttribute('src', './assets/images/icon-plus.svg');
+    faqAnswer[item].classList.remove("faq__answer--active");
+}
+
+function showAnswer(item) {
+    faqIcon[item].setAttribute('src', './assets/images/icon-minus.svg');
+    faqAnswer[item].classList.add("faq__answer--active");
+}
+
